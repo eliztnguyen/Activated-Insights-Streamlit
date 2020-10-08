8e4277a25be6f1412b09d5d2d236645d870d26a1
eliztnguyen <4267981+eliztnguyen@users.noreply.github.com>
2020-10-08 13:30:21 -0400

    Update ActivatedInsights_Streamlit.py
    removed from pd.read_excel():
    engine="openpyxl"

--- ActivatedInsights_Streamlit.py
+++ ActivatedInsights_Streamlit.py
@@ -18,7 +18,7 @@ import re
 #@st.cache(hash_funcs={pandas.core.frame.DataFrame: my_hash_func})
 def readinresults(data):
     '''simpler readin function for excel file'''
-    df = pd.read_excel(data, engine="openpyxl")
+    df = pd.read_excel(data)
     return df
 
 @st.cache
@@ -34,7 +34,7 @@ def readinFile(filepath, extension):
     if extension == "csv":
         tempdf = pd.read_csv(filepath)
     elif extension == "xlsx" or extension == "xls":
-        tempdf = pd.read_excel(filepath, engine="openpyxl")
+        tempdf = pd.read_excel(filepath)
     return tempdf
 
 
